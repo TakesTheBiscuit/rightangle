ffb1ec55e2ef80ec1f7cc93528a1fa58d6f4f640
Paul Drage <paul@e-swoosh.co.uk>
2019-05-14 22:43:50 +0100

    playing

---
+++ appy/ts/main.ts
@@ -0,0 +1,51 @@
+
+export {}
+
+let message = 'hello bantzs';
+console.log(message);
+
+const title = 'code bants';
+console.log(title);
+
+let isMong:boolean = true;
+let totalMong:number = 0;
+let mongName:string = 'Bish bash';
+
+let sentence: string = `My name is ${mongName}
+I am a total mong`;
+
+console.log(sentence);
+
+let n: null = null;
+let u:undefined = undefined;
+
+let list1: number[] = [1,10,20];
+let list2: Array<number> = [2,4,6];
+
+let person1: [string, number] = ['chris', 22];
+
+enum Color{Red = 4, Green, Bloo};
+let c: Color = Color.Bloo;
+console.log(c);
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Bish bash';
+randomValue = mongName;
+randomValue = c;
+
+
+
+let a;
+a = 10;
+
+a = 'a';
+a = true;
+
+let baa = 20;
+baa = true;
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
